a579e978b06076aa5c868ddade09d1b67ba8df21
NGUYEN DUC KIEN <kien.nd@kiotviet.com>
2022-10-21 18:00:55 +0700

    File CRUD

---
+++ backend/src/main/java/com/csdlpt/backend/model/file/FileList.java
@@ -0,0 +1,6 @@
+package com.csdlpt.backend.model.file;
+
+import java.util.ArrayList;
+
+public class FileList extends ArrayList<FileRes> {
+}
---
+++ backend/src/main/java/com/csdlpt/backend/model/file/FileReq.java
@@ -0,0 +1,13 @@
+package com.csdlpt.backend.model.file;
+
+public class FileReq {
+    private int productId;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+}
